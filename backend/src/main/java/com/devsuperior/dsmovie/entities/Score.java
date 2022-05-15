729042e83321e54718b964a8b5526d9faa33257f
Michael Douglas de Lima <michaeldl@uol.com.br>
2022-05-15 19:38:46 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -7,20 +7,20 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_score")
 public class Score {
-	
+
 	@EmbeddedId
-	
 	private ScorePK id = new ScorePK();
+
 	private Double value;
-	
+
 	public Score() {
-		
+
 	}
-	
+
 	public void setMovie(Movie movie) {
 		id.setMovie(movie);
 	}
-	
+
 	public void setUser(User user) {
 		id.setUser(user);
 	}
